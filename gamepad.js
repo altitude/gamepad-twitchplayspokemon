b9411df9c6f8601a0d6463a81e5fe2c8c20aefdd
altitude-labs <salaun.clement@gmail.com>
2014-02-19 01:47:06 +0100

    fix

--- gamepad.js
+++ gamepad.js
@@ -1,10 +1,10 @@
-var gp_view = '<div id="gamepad">
-		<button class="button-up button-arrow" action="up">U</button>
-		<button class="button-down button-arrow" action="down">D</button>
-		<button class="button-left button-arrow" action="left">L</button>
-		<button class="button-right button-arrow" action="right">R</button>
-		<button class="button-a button-ab" action="a">A</button>
-		<button class="button-b button-ab" action="b">B</button>
+var gp_view = '<div id="gamepad">\
+		<button class="button-up button-arrow" action="up">U</button>\
+		<button class="button-down button-arrow" action="down">D</button>\
+		<button class="button-left button-arrow" action="left">L</button>\
+		<button class="button-right button-arrow" action="right">R</button>\
+		<button class="button-a button-ab" action="a">A</button>\
+		<button class="button-b button-ab" action="b">B</button>\
 	</div>';
 
 // apply gamepad stylesheet
